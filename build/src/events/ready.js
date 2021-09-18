2f9c356f3aa51b08cb91df7243fa108ad92f6776
Artem <trystatik@icloud.com>
2021-09-18 06:23:47 +0300

    deleted build && procfile

--- build/src/events/ready.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const event_1 = require("../structures/event");
-module.exports = new event_1.Event('ready', () => {
-    console.log('Bot is ready!');
-});
