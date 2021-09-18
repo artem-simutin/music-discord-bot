3465575f73450782b648ffb13ba5e1765b55c7be
Artem <trystatik@icloud.com>
2021-09-19 01:10:07 +0300

    Updated viewa formatter

--- src/services/viewsConverter.ts
+++ src/services/viewsConverter.ts
@@ -5,13 +5,16 @@ export const formatViews = (num: number | string): string => {
   const count = Number(num)
 
   // If count is not number => return no info
-  if (!count) 'No information'
+  if (!count) return 'No information'
 
   // convert to K for number from > 1000 < 1 million
-  if (count > 999 && count < 1e6) (count / 1e3).toFixed(1) + 'K'
+  if (count > 999 && count < 1e6) return (count / 1e3).toFixed(1) + 'K'
 
   // convert to M for number from > 1 million
-  if (count > 1e6) (count / 1e6).toFixed(1) + 'M'
+  if (count > 1e6 && count < 1e9) return (count / 1e6).toFixed(1) + 'M'
+
+  // convert to B for number from > 1 billion
+  if (count > 1e9) return (count / 1e9).toFixed(1) + 'B'
 
   // if value < 1000, nothing to do
   return count.toString()
