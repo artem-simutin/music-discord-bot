44c27f3f73d43d647b60550ca12bf9a99f4c93c3
Artjoms S <artjoms.s@develops.today>
2021-12-25 18:35:25 +0200

    improvement(services/parseDuration): implemented duration parse in hours

--- src/services/parceDuration.ts
+++ src/services/parceDuration.ts
@@ -9,6 +9,22 @@ export const parseDuration = (duration: string | number): string => {
   const minutes = Math.floor(seconds / 60)
   const remainingSeconds = seconds - minutes * 60
 
+  if (minutes > 60) {
+    const hours = Math.floor(minutes / 60)
+    const remainingMinutes = Math.floor((seconds - hours * 3600) / 60)
+    const remainingSeconds = seconds - hours * 3600 - remainingMinutes * 60
+
+    return `${hours}:${
+      remainingMinutes.toString().length === 1
+        ? `0${remainingMinutes}`
+        : remainingMinutes.toString()
+    }:${
+      remainingSeconds.toString().length === 1
+        ? `0${remainingSeconds}`
+        : remainingSeconds.toString()
+    }`
+  }
+
   return `${minutes}:${
     remainingSeconds.toString().length === 1
       ? `0${remainingSeconds}`
